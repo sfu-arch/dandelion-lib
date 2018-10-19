86e10127e4c442e762f5811713996334a95b03cc
Amirali Sharifian <am.sharifian@gmail.com>
2018-10-18 23:34:59 -0700

    Add new Phi, const and branch node

--- src/main/scala/node/PhiNode.scala
+++ src/main/scala/node/PhiNode.scala
@@ -23,6 +23,27 @@ class PhiNodeIO(NumInputs: Int, NumOuts: Int)
   val Mask = Flipped(Decoupled(UInt(NumInputs.W)))
 }
 
+abstract class PhiFastNodeIO(val NumInputs: Int = 2, val NumOutputs: Int = 1, val ID: Int)
+                            (implicit val p: Parameters)
+  extends Module with CoreParams with UniformPrintfs {
+
+  val io = IO(new Bundle {
+    //Control signal
+    val enable = Flipped(Decoupled(new ControlBundle))
+
+    // Vector input
+    val InData = Vec(NumInputs, Flipped(Decoupled(new DataBundle)))
+
+    // Predicate mask comming from the basic block
+    val Mask = Flipped(Decoupled(UInt(NumInputs.W)))
+
+    //Output
+    val Out = Vec(NumOutputs, Decoupled(new DataBundle))
+  })
+}
+
+
+@deprecated("Use PhiFastNode2 instead", "1.0")
 class PhiNode(NumInputs: Int,
               NumOuts: Int,
               ID: Int)
@@ -118,7 +139,10 @@ class PhiNode(NumInputs: Int,
         Reset()
 
         //Print output
-        printf("[LOG] " + "[" + module_name + "] [TID->%d] " + node_name + ": Output fired @ %d, Value: %d\n", out_data_R.taskID, cycleCount, out_data_R.data)
+        if (log) {
+          printf("[LOG] " + "[" + module_name + "] [TID->%d] "
+            + node_name + ": Output fired @ %d, Value: %d\n", out_data_R.taskID, cycleCount, out_data_R.data)
+        }
 
 
       }
@@ -150,25 +174,14 @@ class PhiNode(NumInputs: Int,
   * @param name
   * @param file
   */
-class PhiFastNode(val NumInputs: Int = 2, val NumOutputs: Int = 1, val ID: Int)
-                 (implicit val p: Parameters,
+
+
+@deprecated("Use PhiFastNode2 instead", "1.0")
+class PhiFastNode(NumInputs: Int = 2, NumOutputs: Int = 1, ID: Int)
+                 (implicit p: Parameters,
                   name: sourcecode.Name,
                   file: sourcecode.File)
-  extends Module with CoreParams with UniformPrintfs {
-
-  val io = IO(new Bundle {
-    //Control signal
-    val enable = Flipped(Decoupled(new ControlBundle))
-
-    // Vector input
-    val InData = Vec(NumInputs, Flipped(Decoupled(new DataBundle)))
-
-    // Predicate mask comming from the basic block
-    val Mask = Flipped(Decoupled(UInt(NumInputs.W)))
-
-    //Output
-    val Out = Vec(NumOutputs, Decoupled(new DataBundle))
-  })
+  extends PhiFastNodeIO(NumInputs, NumOutputs, ID)(p) {
 
   // Printf debugging
   override val printfSigil = "Node (PHIFast) ID: " + ID + " "
@@ -205,7 +218,7 @@ class PhiFastNode(val NumInputs: Int = 2, val NumOutputs: Int = 1, val ID: Int)
 
   // Latching enable value
   io.enable.ready := ~enable_valid_R
-  when(io.Mask.fire()) {
+  when(io.enable.fire()) {
     enable_R <> io.enable.bits
     enable_valid_R := true.B
   }
@@ -213,16 +226,22 @@ class PhiFastNode(val NumInputs: Int = 2, val NumOutputs: Int = 1, val ID: Int)
 
   for (i <- 0 until NumInputs) {
     io.InData(i).ready := ~in_data_valid_R(i)
-    when(io.InData(i).fire && io.InData(i).bits.predicate) {
-      //Make sure that we only pick predicated values!
-      in_data_R(i) <> io.InData(i).bits
-      in_data_valid_R(i) := true.B
+    when(io.InData(i).fire) {
+      when(io.InData(i).bits.predicate && io.InData(i).bits.predicate) {
+        //Make sure that we only pick predicated values!
+        in_data_R(i) <> io.InData(i).bits
+        in_data_valid_R(i) := true.B
+      }.otherwise {
+        in_data_R(i) := DataBundle.default
+        in_data_valid_R(i) := false.B
+      }
     }
   }
 
   val mask_value =
-    (io.Mask.bits.data & Fill(NumInputs, io.Mask.valid)) | (mask_R & Fill(NumInputs, mask_valid_R))
+    (io.Mask.bits & Fill(NumInputs, io.Mask.valid)) | (mask_R & Fill(NumInputs, mask_valid_R))
 
+  //val sel = OHToUInt(Reverse(mask_value))
   val sel = OHToUInt(mask_value)
 
 
@@ -260,12 +279,11 @@ class PhiFastNode(val NumInputs: Int = 2, val NumOutputs: Int = 1, val ID: Int)
         when(enable_input) {
           when(in_data_valid_R(sel) || io.InData(sel).fire) {
             io.Out.foreach(_.valid := true.B)
-
             when(io.Out.map(_.fire).reduce(_ & _)) {
               in_data_R.foreach(_ := DataBundle.default)
               in_data_valid_R.foreach(_ := false.B)
 
-              //mask_R := 0.U
+              mask_R := 0.U
               mask_valid_R := false.B
 
               enable_R := ControlBundle.default
@@ -282,9 +300,11 @@ class PhiFastNode(val NumInputs: Int = 2, val NumOutputs: Int = 1, val ID: Int)
             }
 
             //Print output
-            printf("[LOG] " + "[" + module_name + "] [TID->%d] "
-              + node_name + ": Output fired @ %d, Value: %d\n",
-              io.InData(sel).bits.taskID, cycleCount, io.InData(sel).bits.data)
+            if (log) {
+              printf("[LOG] " + "[" + module_name + "] [TID->%d] "
+                + node_name + ": Output fired @ %d, Value: %d\n",
+                io.InData(sel).bits.taskID, cycleCount, select_input)
+            }
           }
         }.otherwise {
           // Wireing outputs
@@ -294,49 +314,19 @@ class PhiFastNode(val NumInputs: Int = 2, val NumOutputs: Int = 1, val ID: Int)
           io.Out.map(_.valid) foreach (_ := false.B)
           io.Out.foreach(_.valid := true.B)
 
-          when(io.Out.map(_.fire).reduce(_ & _)) {
+          when(fire_mask.reduce(_ & _)) {
             enable_R := ControlBundle.default
             enable_valid_R := false.B
 
             fire_R.foreach(_ := false.B)
+
+            mask_R := 0.U
 
             state := s_idle
           }.otherwise {
             state := s_not_predicated
           }
         }
-      }
-
-
-      when((enable_valid_R || io.enable.fire)
-        && (in_data_valid_R(sel) || io.InData(sel).fire)) {
-
-        io.Out.foreach(_.valid := true.B)
-
-        when(io.Out.map(_.fire).reduce(_ & _)) {
-          in_data_R.foreach(_ := DataBundle.default)
-          in_data_valid_R.foreach(_ := false.B)
-
-          //mask_R := 0.U
-          mask_valid_R := false.B
-
-          enable_R := ControlBundle.default
-          enable_valid_R := false.B
-
-          out_valid_R.foreach(_ := false.B)
-
-          fire_R.foreach(_ := false.B)
-
-          state := s_idle
-
-        }.otherwise {
-          state := s_fire
-        }
-
-        //Print output
-        printf("[LOG] " + "[" + module_name + "] [TID->%d] "
-          + node_name + ": Output fired @ %d, Value: %d\n",
-          io.InData(sel).bits.taskID, cycleCount, io.InData(sel).bits.data)
       }
     }
 
@@ -377,6 +367,231 @@ class PhiFastNode(val NumInputs: Int = 2, val NumOutputs: Int = 1, val ID: Int)
         enable_valid_R := false.B
 
         fire_R.foreach(_ := false.B)
+
+        mask_R := 0.U
+
+        state := s_idle
+      }
+    }
+  }
+
+}
+
+
+/**
+  * A fast version of phi node.
+  * The ouput is fired as soon as all the inputs
+  * are available.
+  *
+  * @note: These are design assumptions:
+  *        1) At each instance, there is only one input signal which is predicated
+  *        there is only one exception.
+  *        2) The only exception is the case which one of the input is constant
+  *        and because of our design constant is always fired as a first node
+  *        and it has only one output. Therefore, whenever we want to restart
+  *        the states, we reste all the registers, and by this way we make sure
+  *        that nothing is latched.
+  *        3) If Phi node itself is not predicated, we restart all the registers and
+  *        fire the output with zero predication.
+  * @param NumInputs
+  * @param NumOuts
+  * @param ID
+  * @param p
+  * @param name
+  * @param file
+  */
+class PhiFastNode2(NumInputs: Int = 2, NumOutputs: Int = 1, ID: Int)
+                  (implicit p: Parameters,
+                   name: sourcecode.Name,
+                   file: sourcecode.File)
+  extends PhiFastNodeIO(NumInputs, NumOutputs, ID)(p) {
+
+  // Printf debugging
+  override val printfSigil = "Node (PHIFast) ID: " + ID + " "
+  val (cycleCount, _) = Counter(true.B, 32 * 1024)
+
+  // Printf debugging
+  val node_name = name.value
+  val module_name = file.value.split("/").tail.last.split("\\.").head.capitalize
+
+  // Data Inputs
+  val in_data_R = RegInit(VecInit(Seq.fill(NumInputs)(DataBundle.default)))
+  val in_data_valid_R = RegInit(VecInit(Seq.fill(NumInputs)(false.B)))
+
+  // Enable Inputs
+  val enable_R = RegInit(ControlBundle.default)
+  val enable_valid_R = RegInit(false.B)
+
+  // Mask Input
+  val mask_R = RegInit(0.U(NumInputs.W))
+  val mask_valid_R = RegInit(false.B)
+
+  // Latching output data
+  val out_valid_R = Seq.fill(NumOutputs)(RegInit(false.B))
+
+  val fire_R = Seq.fill(NumOutputs)(RegInit(false.B))
+
+  // Latching Mask value
+  io.Mask.ready := ~mask_valid_R
+  when(io.Mask.fire()) {
+    mask_R := io.Mask.bits
+    mask_valid_R := true.B
+  }
+
+  // Latching enable value
+  io.enable.ready := ~enable_valid_R
+  when(io.enable.fire()) {
+    enable_R <> io.enable.bits
+    enable_valid_R := true.B
+  }
+
+
+  for (i <- 0 until NumInputs) {
+    io.InData(i).ready := ~in_data_valid_R(i)
+    when(io.InData(i).fire) {
+      when(io.InData(i).bits.predicate) {
+        //Make sure that we only pick predicated values!
+        in_data_R(i) <> io.InData(i).bits
+        in_data_valid_R(i) := true.B
+      }.otherwise {
+        in_data_R(i) := DataBundle.default
+        in_data_valid_R(i) := false.B
+      }
+    }
+  }
+
+  //val sel = OHToUInt(Reverse(mask_value))
+  val sel = OHToUInt(mask_R)
+
+  val select_input = in_data_R(sel).data
+  val select_predicate = in_data_R(sel).predicate
+
+  val enable_input = enable_R.control
+
+  val task_input = (io.enable.bits.taskID | enable_R.taskID)
+
+  // Wiring outputs default value
+  io.Out.map(_.bits) foreach (_.data := 0.U)
+  io.Out.map(_.bits) foreach (_.predicate := false.B)
+  io.Out.map(_.bits) foreach (_.taskID := 0.U)
+  io.Out.map(_.valid) foreach (_ := false.B)
+
+
+  for (i <- 0 until NumOutputs) {
+    when(io.Out(i).fire) {
+      fire_R(i) := true.B
+      out_valid_R(i) := false.B
+    }
+  }
+
+  //Getting mask for fired nodes
+  val fire_mask = (fire_R zip io.Out.map(_.fire)).map { case (a, b) => a | b }
+
+  //Output register
+  val s_idle :: s_fire :: s_not_predicated :: Nil = Enum(3)
+  val state = RegInit(s_idle)
+
+  switch(state) {
+    is(s_idle) {
+      when(enable_valid_R) {
+        when(enable_R.control) {
+          when(in_data_valid_R(sel)) {
+            out_valid_R foreach {
+              _ := true.B
+            }
+            state := s_fire
+
+            //Print output
+            if (log) {
+              printf("[LOG] " + "[" + module_name + "] [TID->%d] "
+                + node_name + ": Output fired @ %d, Value: %d\n",
+                io.InData(sel).bits.taskID, cycleCount, select_input)
+            }
+          }
+        }.otherwise {
+          out_valid_R foreach {
+            _ := true.B
+          }
+          state := s_not_predicated
+          //Print output
+          if (log) {
+            printf("[LOG] " + "[" + module_name + "] [TID->%d] "
+              + node_name + ": Output flushed @ %d, Value: %d\n",
+              io.InData(sel).bits.taskID, cycleCount, select_input)
+          }
+        }
+      }
+    }
+
+    is(s_fire) {
+      io.Out.map(_.bits) foreach (_ := in_data_R(sel))
+      (io.Out.map(_.valid) zip out_valid_R).map { case (a, b) => a := b }
+
+      when(fire_mask.reduce(_ & _)) {
+
+        /** @note: In this case whenever all the GEP is fired we
+          *       restart all the latched values. But it may be cases
+          *       that because of pipelining we have latched an interation ahead
+          *       and if we may reset the latches values we lost the value.
+          *       I'm not sure when this case can happen!
+         */
+        // in_data_R(sel) := DataBundle.default
+        // in_data_valid_R(sel) := false.B
+        in_data_R foreach {
+          _ := DataBundle.default
+        }
+        in_data_valid_R foreach {
+          _ := false.B
+        }
+
+        mask_R := 0.U
+        mask_valid_R := false.B
+
+        enable_R := ControlBundle.default
+        enable_valid_R := false.B
+
+        out_valid_R foreach {
+          _ := false.B
+        }
+
+        fire_R foreach {
+          _ := false.B
+        }
+
+        state := s_idle
+
+      }
+
+    }
+    is(s_not_predicated) {
+      io.Out.map(_.bits) foreach (_.data := 0.U)
+      io.Out.map(_.bits) foreach (_.predicate := enable_input)
+      io.Out.map(_.bits) foreach (_.taskID := task_input)
+      io.Out.map(_.valid) foreach (_ := false.B)
+      (io.Out.map(_.valid) zip out_valid_R).map { case (a, b) => a := b }
+
+      when(io.Out.map(_.fire).reduce(_ & _)) {
+
+        //        in_data_R(sel) := DataBundle.default
+        //        in_data_valid_R(sel) := false.B
+        in_data_R foreach {
+          _ := DataBundle.default
+        }
+        in_data_valid_R foreach {
+          _ := false.B
+        }
+
+        enable_R := ControlBundle.default
+        enable_valid_R := false.B
+
+        fire_R.foreach(_ := false.B)
+
+        mask_R := 0.U
+        mask_valid_R := false.B
+
+        out_valid_R foreach {
+          _ := true.B
+        }
 
         state := s_idle
       }
